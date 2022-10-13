6488b1324f4ebd6c2cf8a27d2f675fd5e6d6f29c
emarex1 <emmanuelawoh0@gmail.com>
2022-10-13 11:16:21 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* entry point of the program */
+
+int main(void)
+{
+
+	/* the print function*/
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return(0);
+
+}
