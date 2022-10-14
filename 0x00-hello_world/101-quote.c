431fbe3b5d4c9e7f3534c8c838e581e115a9ca97
emarex1 <emmanuelawoh0@gmail.com>
2022-10-14 15:40:33 -0700

    print to erro

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,21 @@
 #include <unistd.h>
 
 /**
-* main - the entry point of the programe
-*
-* Return: always returns 1.
-*/
+ * main - Entry Point
+ *
+ * Description: prints a string to the standard error
+ *
+ * Return: Always 1(Error)
+ */
 
 int main(void)
 
 {
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
 
-	Return (1);
+	write(2,  err, 59);
+
+	return (1);
 
 }
