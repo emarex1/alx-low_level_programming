dcf7defd2e4511534b0d730bdc7f289f1105f506
emarex1 <emmanuelawoh0@gmail.com>
2022-10-29 00:00:51 -0700

    modified

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
-ar *_strcat(char *dest, char *src);
+int _putchar(char c);
+
+char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
-
-int _putchar(char c);
 
 char *_strncpy(char *dest, char *src, int n);
 
