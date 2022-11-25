e6f9a8c342747acf4f0aa81aba0091f2cf8ccab8
emarex1 <emmanuelawoh0@gmail.com>
2022-11-25 03:32:50 -0800

    fixed betty issue

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,22 +20,22 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new->str = strdup(str);
 
-	for (nchar = 0; str[nchar]; nchar++);
+	for (nchar = 0; str[nchar]; nchar++)
 
 	new->len = nchar;
 	new->next = NULL;
-    temp = *head;
+	temp = *head;
 
-    if (temp == NULL)
-    {
-        *head = new;
-    }
-    else
-    {
-        while (temp->next != NULL)
-            temp = temp->next;
-        temp->next = new;
-    }
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+	while (temp->next != NULL)
+		temp = temp->next;
+		temp->next = new;
+	}
 
-    return (*head);
+	return (*head);
 }
