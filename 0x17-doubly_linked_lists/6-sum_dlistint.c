21fbd1824c7df87828e5daecefe148da05930731
emarex1 <emmanuelawoh0@gmail.com>
2023-01-13 19:45:25 +0100

    sum of n of linked lists

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t list
+ * @head: pointer to head of the list
+ * Return: sum of all nodes or 0
+ **/
+
+int sum_dlistint(dlistint_t *head);
+
+{
+
+    current = *head;
+    int sum = 0;
+    while(current)
+    {
+       sum += current->data;
+       current = current->next;
+    }
+    return(sum);
+}
