284e37e59be6e5e66990949c196d5878eceebadc
emarex1 <emmanuelawoh0@gmail.com>
2023-01-13 19:14:27 +0100

    get nth list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,11 +14,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-
 	while (head != NULL)
 	{
 		if (index == count)
