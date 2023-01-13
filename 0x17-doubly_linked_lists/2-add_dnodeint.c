1178afad2081db74d4a41a877fb16cdfd7716993
emarex1 <emmanuelawoh0@gmail.com>
2023-01-13 16:37:58 +0100

    fixed betty error

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,9 +16,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node ->n = n;
-	new_node ->prev = NULL;
-	new_node ->next = *head;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
 	if (*head != NULL)
 		(*head)->prev = new_node;
 	*head = new_node;
