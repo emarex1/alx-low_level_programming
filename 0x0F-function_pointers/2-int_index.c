333100496d1210c69468775899e5874fb035f646
emarex1 <emmanuelawoh0@gmail.com>
2023-05-19 17:06:08 +0100

    fixed an error

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,4 @@
 #include "function_pointers.h"
-
-
 /**
  * int_index - searches for an integer
  * @array: input integer array.
@@ -14,16 +12,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
+int i;
 
-    if (array == NULL || cmp == NULL || size <= 0)
-        return -1;
+if (size <= 0)
+return (-1);
+if (array == NULL || cmp == NULL)
+return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]) != 0)
-            return i;
-    }
-
-    return -1;
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) == 1)
+return (i);
+}
+return (-1);
 }
