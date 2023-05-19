6ac6cfbc920db01a9d6e86c064eb41bf43880752
emarex1 <emmanuelawoh0@gmail.com>
2023-05-19 16:34:17 +0100

    header for task 3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,17 +11,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
+	int i;
 
-    if (array == NULL || cmp == NULL || size <= 0)
-        return -1;
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]) != 0)
-            return i;
-    }
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+	}
 
-    return -1;
+	return (-1);
 }
-
