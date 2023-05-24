582ba35ca6332e135c649326705aaeadb59babaa
emarex1 <emmanuelawoh0@gmail.com>
2023-05-24 21:24:37 +0100

    error fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t nodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		nnodes++;
+		nodes++;
 	}
-	return (nnodes);
+	return (nodes);
 }
